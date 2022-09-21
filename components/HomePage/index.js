51f4246f2dc4e7f1d83f0acc8c4e6cf7f4fd153d
hoainambrvt2001 <hoainambrvt2001@gmail.com>
2022-09-21 08:41:35 +0700

    init

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,5 +1,7 @@
+import classes from "./index.module.scss";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return <div className={classes.Wrapper}>Home Page</div>;
 };
 
 export default HomePage;
