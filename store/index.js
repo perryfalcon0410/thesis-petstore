8126a26fbbbcff05e998d6810fdc5a91530d296f
tuniss1 <77372423+tuniss1@users.noreply.github.com>
2022-11-05 12:42:03 +0700

    Add userslice

--- store/index.js
+++ store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CheckoutSlice from "./reducers/checkoutSlice";
+import UserSlice from "./reducers/userSlice";
 
 const store = configureStore({
   reducer: {
     checkout: CheckoutSlice.reducer,
+    user: UserSlice.reducer
   },
   devTools: true,
 });
