2edd850597c53e406fd4f7c2f30191df7702bf0e
Huỳnh Kim Hưng <infernoid0410@gmail.com>
2023-06-02 23:56:38 +0700

    eslint

--- components/OrderManagement/OrderList/index.js
+++ components/OrderManagement/OrderList/index.js
@@ -58,11 +58,55 @@ const OrderList = ({ orderList }) => {
             return (
               <div className="card" key={order.id}>
                 <div className="top">
-                  {/* ... */}
+                  <div className="status">
+                    <DeliveryIcon width={20} height={20} stroke={statusColor[order.status]} fill={'white'} />
+                    <p style={{ color: statusColor[order.status] }}>{statusTitle[order.status]}</p>
+                    {/* <div className="column-divider"></div>
+                  <p style={{ color: '#DD583B' }}>DELIVERED</p> */}
+                  </div>
+                  <div className="divider"></div>
+                  <div className="product-container">
+                    {order.cart.map((product, idx) => {
+                      if (idx + 1 > LIMIT_PRODUCT_ORDER_LIST) return <div key={idx}></div>
+                      return (
+                        <div key={product.id} className="product">
+                          <div className="info-container">
+                            <div className="image">
+                              <Image
+                                src={product.images.length ? product.images[0].url : '/images/no-image.png'}
+                                alt={product.images.length !== 0 ? product.images[0].image_name : 'product image'}
+                                width={104}
+                                height={104}
+                              />
+                            </div>
+                            <div className="info">
+                              <p>{product.name}</p>
+                              <p>x{product.quantity}</p>
+                            </div>
+                          </div>
+                          <div className="cost">
+                            <p>${product.price * product.quantity}</p>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </div>
                 </div>
                 <div className="divider"></div>
                 <div className="bottom">
-                  {/* ... */}
+                  <div className="shipping-fee">
+                    <p style={{ fontWeight: 700, fontSize: 16 }}>
+                      Shipping Fee: <span style={{ color: '#dd583b' }}>${order.shippingFee.toFixed(2)}</span>
+                    </p>
+                  </div>
+                  <div className="total-cost">
+                    <p style={{ fontWeight: 700, fontSize: 20 }}>
+                      Total: <span style={{ color: '#dd583b' }}>${order.totalPrice.toFixed(2)}</span>
+                    </p>
+                  </div>
+                  <Link href={`/order-management/${order._id}`}>
+                    <a>View detail</a>
+                  </Link>
                 </div>
               </div>
             )
